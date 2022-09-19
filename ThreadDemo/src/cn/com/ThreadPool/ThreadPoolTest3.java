13494db5a6abae61b6d19c49e01a9ec629ed96ca
lixin <lixin@DESKTOP-RVED93C>
2022-09-19 22:05:46 +0800

    线程池

--- ThreadDemo/src/cn/com/ThreadPool/ThreadPoolTest3.java
+++ ThreadDemo/src/cn/com/ThreadPool/ThreadPoolTest3.java
@@ -7,11 +7,13 @@ import java.util.concurrent.*;
  * Create by 2022/9/19 21:37
  *
  * 使用Executors的工具方法直接的到一个线程池对象
- * 缺点：不能抗住大量任务的操作，会自己创建大量线程
+ * 缺点：1.不能抗住大量任务的操作，2.会自己创建大量线程
  */
 public class ThreadPoolTest3 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService pool =  Executors.newFixedThreadPool(3);
+        ExecutorService pool =  Executors.newFixedThreadPool(3); //1.不能抗住大量任务的操作
+        //ExecutorService pool =  Executors.newCachedThreadPool(); 遇到大的线程会自己创建大量线程
+
         pool.execute(new MyRunnable());
         pool.execute(new MyRunnable());
         pool.execute(new MyRunnable());
